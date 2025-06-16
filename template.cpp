c1820a5da48827ab53667983a7c1b1d9847345fa
tottoro2008 <shoot.koe@gmail.com>
2025-06-16 23:56:18 +0900

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,64 @@
+#ifdef INCLUDED_MAIN
+
+int solve(){
+    
+    return 0;
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int T = 1;
+    while (T--) {
+        solve();
+    }
+    return 0;
+}
+
+#else
+
+/*****    Template area    *****/
+#include <iostream>
+#include <vector>
+#include <string>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <algorithm>
+#include <functional>
+#include <cmath>
+#include <numeric>
+#include <limits>
+#include <iterator>
+#include <cassert>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <random>
+#include <sstream>
+#include <chrono>
+#include <stdexcept>
+#include <set>
+#include <map>
+
+using namespace std;
+using ll = long long;
+using vi vector<int>;
+
+
+
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define all(x) (x).begin(), (x).end()
+#define ssort(v) sort(all(v))
+#define lsort(v) sort(all(v), greater<>())
+#define yes(cond) cout << ((cond) ? "Yes" : "No") << endl
+
+/**************************************/
+
+
+
+#define INCLUDED_MAIN
+#include __FILE__
+#endif
